1e56435d4763f2a8e2d3da95e4dba9e0151f780a
Andrew Shearer <andrewshearerdev@gmail.com>
2023-08-26 15:14:27 -0700

    added generateStaticParams

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,4 +1,23 @@
+import { notFound } from "next/navigation";
 import type { Ticket } from "@/types";
+
+export const dynamicParams = true; // default val = true
+
+type TicketId = {
+  id: string;
+};
+
+export const generateStaticParams = async (): Promise<TicketId[]> => {
+  const res = await fetch("http://localhost:4000/tickets");
+  const tickets = await res.json();
+
+  // ids: [{ id: 'PYHz3gka' }, { id: 'aQeD00iT' }, { id: 'rQKgM79M' }]
+  const ids = tickets.map((ticket: Ticket) => ({
+    id: ticket.id
+  }));
+
+  return ids;
+};
 
 const getTicket = async (id: string): Promise<Ticket> => {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
@@ -6,6 +25,10 @@ const getTicket = async (id: string): Promise<Ticket> => {
       revalidate: 60
     }
   });
+
+  if (!res.ok) {
+    notFound();
+  }
 
   return res.json();
 };
