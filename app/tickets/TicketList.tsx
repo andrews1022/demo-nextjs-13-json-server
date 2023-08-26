39683b12654acbd4ff9c1e587b528e4ce652bf75
Andrew Shearer <andrewshearerdev@gmail.com>
2023-08-26 14:20:50 -0700

    lesson 6 updates
    - installed json-server and created npm script to easily start it up
    - added db.json with dummy data
    - updated tickets page jsx
    - created TicketList component to fetch data from json-server
    - created Ticket type

---
+++ app/tickets/TicketList.tsx
@@ -0,0 +1,34 @@
+import { Ticket } from "@/types";
+
+const getTickets = async () => {
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      // value is in seconds
+      revalidate: 0 // use 0 to opt out of using cache - will always be fresh
+    }
+  });
+
+  return res.json();
+};
+
+const TicketList = async () => {
+  const tickets = await getTickets();
+
+  return (
+    <div>
+      <h2>Tickets</h2>
+
+      {tickets.map((ticket: Ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 200)}...</p>
+          <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+        </div>
+      ))}
+
+      {!tickets.length && <p className="text-center">There are no open tickets</p>}
+    </div>
+  );
+};
+
+export default TicketList;
---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export type Ticket = {
+  body: string;
+  id: string;
+  priority: string;
+  title: string;
+  user_email: string;
+};
